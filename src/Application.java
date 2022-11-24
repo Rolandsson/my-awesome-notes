02fb099c918c0f3d91a37f689948b8c63b8b1f89
Jakob Rolandsson <jakobrolandsson@Jakobs-Air.local>
2022-11-24 11:42:37 +0100

    added while loop to notes

--- src/Application.java
+++ src/Application.java
@@ -7,10 +7,13 @@ public class Application {
   public static void main(String[] args) {
     List<String> notes = new ArrayList<>();
     Scanner scanner = new Scanner(System.in);
+    boolean isDone = false;
 
-    System.out.print("Add new noteeee: ");
-    String note = scanner.nextLine();
+    while(isDone) {
+      System.out.print("Add new noteeee: ");
+      String note = scanner.nextLine();
+      notes.add(note);
+    }
 
-    notes.add(note);
   }
 }
